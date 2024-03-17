e382eaa737f76026ffe63d999e03e94627475d86
Collin <cshook16@hotmail.com>
2024-03-16 21:26:48 -0600

    add sqflite
    Added sqflite basic functions into app, also added items json file

--- capstone_app/lib/main.dart
+++ capstone_app/lib/main.dart
@@ -1,33 +1,27 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'dart:io';
+
+void main()
+{
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget
+{
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pantry Tracker App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -36,17 +30,10 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget
+{
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -54,72 +41,185 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage>
+{
+  DatabaseManager dbm = DatabaseManager();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  _MyHomePageState()
+  {
+    initializeDatabase();
+  }
+
+  initializeDatabase() async
+  {
+    await dbm.dbSetup(); // Await the asynchronous method
+    insertRandomData(25);
+    dbm.showData();
+
+    dbm.clearTable();
+  }
+
+  // function to populate test data into db
+  insertRandomData(int amountToInsert)
+  {
+    for(int i = 0; i < amountToInsert; i++)
+    {
+      Random RNG = Random();
+
+      dbm.insertItemIntoDB(
+          "test$i",
+          "testCategory$i",
+          DateTime.now(),
+          DateTime.now().add(
+              Duration(
+                  days: RNG.nextInt(365)
+              )
+          )
+      );
+    }
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext context)
+  {
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: ListView(
+          children: const [
+            Text("test")
+          ],
+        ) // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+}
+
+class DatabaseManager
+{
+  static String dbname = "pantry.db"; // database name
+  static var db; // database variable
+  var path; // path variable
+
+
+  // database setup function
+  dbSetup() async
+  {
+    // get databases dath
+    var databasesPath = await getDatabasesPath();
+
+    // join the database to the path
+    path = join(databasesPath, dbname);
+
+    // db exists
+    if(await File(path).exists())
+    {
+      print("DB exists");
+    }
+
+    // open the database
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async
+      {
+        await createItemTable(db);
+      },
+      
+    );
+  }
+
+
+  /*
+  SQLite data types
+
+  NULL. The value is a NULL value.
+
+  INTEGER. The value is a signed integer, stored in 0, 1, 2, 3, 4, 6, or 8 bytes depending on the magnitude of the value.
+
+  REAL. The value is a floating point value, stored as an 8-byte IEEE floating point number.
+
+  TEXT. The value is a text string, stored using the database encoding (UTF-8, UTF-16BE or UTF-16LE).
+
+  BLOB. The value is a blob of data, stored exactly as it was input.
+  */
+
+
+  // create the item table
+  createItemTable(Database db) async
+  {
+    await db.execute("""
+      
+    CREATE TABLE tbl_item(
+      name            TEXT,
+      category        TEXT,
+      purchase_date   TEXT,
+      exp_date        TEXT,
+      id              INTEGER PRIMARY KEY
+    );
+      
+    """);
+  }
+
+
+  // insert an item into table
+  insertItemIntoDB(String itemName, String itemCategory, DateTime purchaseDate, DateTime expDate) async
+  {
+    String purchaseDateString = dateTimeToString(purchaseDate);
+    String expDateString = dateTimeToString(expDate);
+
+    String xsql = """
+    INSERT INTO tbl_item(name,category,purchase_date,exp_date) VALUES
+      ('$itemName','$itemCategory','$purchaseDateString','$expDateString')
+    """;
+
+    await db.rawInsert(xsql);
+  }
+
+
+  // print data to console
+  showData() async
+  {
+    // when you do a select query in sqflite
+    // it will return a list of mapped data(key, value)
+    // Think of it as each row of data is
+    // a position in the list. And each record is a Map(key, value)
+    List<Map<String, dynamic>> myDataset;
+
+    myDataset = await getItemsInDB(); // put query results in a map
+
+    // iterate through the map and print each item
+    for(int i = 0; i < myDataset.length; i++)
+    {
+      print("${myDataset[i]['name']} ${myDataset[i]['category']} ${myDataset[i]['purchase_date']} ${myDataset[i]['exp_date']} ${myDataset[i]['id']}");
+    }
+
+  }
+
+
+  // remove item from table
+  removeItem(int id) async
+  {
+    String xsql = "DELETE FROM tbl_item WHERE id = $id";
+    await db.execute(xsql);
+  }
+
+
+  // clear the table
+  clearTable() async
+  {
+    String xsql = "DELETE FROM tbl_item";
+    await db.execute(xsql);
+  }
+
+
+  // convert a date time value to a string of YYYY-MM-DD format
+  String dateTimeToString(DateTime dt) => "${dt.year}-${dt.month}-${dt.day}";
+
+  // get all items in table
+  Future getItemsInDB() async => db.rawQuery("SELECT * FROM tbl_item ORDER BY id ASC");
+
+
 }
